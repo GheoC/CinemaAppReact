286115072c88a09f4703be00abf98db34694cd16
gheo <gheorghe.coanta@gmail.com>
2022-12-22 15:16:49 +0200

    create Base Project Structure

--- src/index.js
+++ src/index.js
@@ -1,6 +1,18 @@
 import {createRoot} from "react-dom/client";
+import App from "./containers/App";
+import 'antd/dist/reset.css';
+import {ConfigProvider} from "antd";
+import {BrowserRouter} from "react-router-dom";
+
 
 const root = document.getElementById("root")
 const main = createRoot(root);
 
-main.render(<h2>Hello world</h2>)
+main.render(<h2>
+    <BrowserRouter>
+        <ConfigProvider theme={{token: {colorPrimary: 'red'}}}>
+            <App/>
+        </ConfigProvider>
+    </BrowserRouter>
+
+</h2>)
