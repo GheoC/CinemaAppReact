b51250f6f7fe939f19c867d60af884db48d1cecb
gheo <gheorghe.coanta@gmail.com>
2022-12-24 14:20:06 +0200

    Play with Ant Design Components
    Beautify application.

--- src/containers/Root/Root.jsx
+++ src/containers/Root/Root.jsx
@@ -1,20 +1,61 @@
-import {Button, Layout} from "antd";
+import {Button, Layout, Menu} from "antd";
 import {useAuthContext} from "../../context/AuthProvider/AuthProvider";
 import {PoweroffOutlined} from "@ant-design/icons";
+import {useState} from "react";
 
 function Root() {
-
-    const {logout} = useAuthContext();
+    const {username, logout} = useAuthContext();
+    const [collapsed, setCollapsed] = useState(true);
     return (
-        <Layout>
-            <Layout.Sider style={{background: "#4f202d", color: "white"}}>Slider</Layout.Sider>
+        <Layout className={"container"}>
+            <Layout.Sider collapsedWidth={"0px"} collapsible collapsed={collapsed}
+                          onCollapse={(value) => setCollapsed(value)}
+                          style={{background: "#4f202d", color: "white"}}>Slider</Layout.Sider>
+
             <Layout>
-                <Layout.Header style={{background: "rgb(17,2,2)", alignItems: "right"}}>
-                    <Button icon={<PoweroffOutlined />} type="primary" danger ghost onClick={logout}></Button>
+                <Layout.Header style={{background: "rgb(17,2,2)", height: "120px"}}>
+                    <Menu
+                        style={{backgroundColor: "black", display: "block", fontFamily: "Quicksand", marginTop: "25px"}}
+                        mode="horizontal"
+                        items={[
+                            {
+                                style: {float: "left"},
+                                key: 'title',
+                                label: <p style={{
+                                    marginBottom: "0px",
+                                    marginTop: "0px",
+                                    fontSize: "70px",
+                                    color: "#d3f261"
+                                }}>Cinema GG</p>,
+
+                            },
+                            {
+                                style: {float: "right"},
+                                key: 'logout',
+                                label: <Button size="large" icon={<PoweroffOutlined/>} type="primary" danger ghost
+                                               onClick={logout}></Button>,
+
+                            },
+                            {
+                                style: {float: "right"},
+                                key: 'greet',
+                                label: <p style={{
+                                    marginBottom: "0px",
+                                    marginTop: "0px",
+                                    fontSize: "20px",
+                                    color: "#d3f261"
+                                }}>Hello {username}</p>,
+
+                            }
+                        ]}
+                    ></Menu>
                 </Layout.Header>
-                <Layout.Content style={{innerHeight: "800px"}}>Content</Layout.Content>
-                <Layout.Footer style={{background: "rgba(140,6,8,0.33)"}}>Footer</Layout.Footer>
+
+                <Layout.Content>Content</Layout.Content>
+                <Layout.Footer style={{background: "rgb(17,2,2)"}}>Footer</Layout.Footer>
             </Layout>
+
+
         </Layout>)
 }
 
